8c7facc7a4a52c67d912de0330971f7babe9409d
Eleven Chen <skyhacker@126.com>
2015-11-14 11:13:25 +0800

    Initial Commit

---
+++ BLEChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEChat
+//
+//  Created by Eleven Chen on 15/11/14.
+//  Copyright © 2015年 Eleven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLEChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLEChat
+//
+//  Created by Eleven Chen on 15/11/14.
+//  Copyright © 2015年 Eleven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
